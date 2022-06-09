66f987404f6594b08d5904af74648f2444184d4e
Adam3192 <jeremyadixon@hotmail.com>
2022-06-09 14:13:27 -0500

    lesson 6 - react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from 'react'
+import AboutUs from './AboutUs'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="about-us" element={<AboutUs />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
