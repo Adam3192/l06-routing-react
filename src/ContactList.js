b89dd6510053af9ee36151a6add9b2707af2d6f7
Adam3192 <jeremyadixon@hotmail.com>
2022-06-12 10:54:16 -0500

    lesson 6 - react

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Stack from 'react-bootstrap/Stack'
-import { Link, Outlet } from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import { Link, Outlet, useSearchParams } from 'react-router-dom'
 
 function ContactList(props) {
+  let [searchParams, setSearchParams] = useSearchParams()
+
   function contacts() {
     if (props.contacts === null) return
     return props.contacts.map((contact) => (
@@ -15,9 +19,29 @@ function ContactList(props) {
     ))
   }
 
+  function handleSearch(event) {
+    let value = event.target.value
+    let params = value ? { query: value } : {}
+    setSearchParams(params)
+  }
+
+  function variantFor(name) {
+    let query = searchParams.get('query')
+    return name.search(query) >= 0 ? 'success' : ''
+  }
+
   return (
     <>
       <h1>Contacts</h1>
+      <Form className="w-50">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          value={searchParams.get('query') || ''}
+          onChange={handleSearch}
+        />
+      </Form>
       <Stack direction="horizontal" gap={3}>
         <ListGroup className="w-75">{contacts()}</ListGroup>
         <Outlet />
